ce0cd9bbf7d14f00ee9297fc49e6c540d39ceac1
AkramHourani <49022541+AkramHourani@users.noreply.github.com>
2019-03-31 22:39:05 +1100

    Add files via upload

--- iSCANv7/MapUpdate/bresenham.m
+++ iSCANv7/MapUpdate/bresenham.m
@@ -5,20 +5,22 @@ x1=round(x1); x2=round(x2);
 y1=round(y1); y2=round(y2);
 dx=abs(x2-x1);
 dy=abs(y2-y1);
-steep=abs(dy)>abs(dx);
-if steep
-    t=dx;dx=dy;dy=t;
+steepLine=abs(dy)>abs(dx);
+if steepLine
+    t=dx;
+    dx=dy;
+    dy=t;
 end
 
-%% The main algorithm
+%% Normal cases
 if dy==0
     q=zeros(dx+1,1);
 else
     q=[0;diff(mod((floor(dx/2):-dy:-dy*dx+floor(dx/2))',dx))>=0];
 end
-%and ends here.
-%%
-if steep
+
+%% In case of steep line
+if steepLine
     if y1<=y2
         y= (y1:y2)';
     else
